9233cfa0eeb5b698160a27fbccfdc7fbbd536e48
xiaofei <xiao_fei_li@sina.com>
2018-05-07 15:28:17 +0800

    scanning flash

--- 二维码/QRViewController/QRScanning/QRScanningViewController.m
+++ 二维码/QRViewController/QRScanning/QRScanningViewController.m
@@ -10,8 +10,17 @@
 #import <AVFoundation/AVFoundation.h>
 #import "QRScanningAnswerViewController.h"
 
+#define kWidth      [UIScreen mainScreen].bounds.size.width
+#define kHeight     [UIScreen mainScreen].bounds.size.height
+
+#define rectangleX      (kWidth - 200)/2
+#define rectangleY      100
+#define rectangleW      200
+#define rectangleH      200
+
 @interface QRScanningViewController ()<AVCaptureMetadataOutputObjectsDelegate>
 
+@property (nonatomic, strong)UIView *inView;
 @property (nonatomic, strong)AVCaptureVideoPreviewLayer *previewLayer;
 @property (nonatomic, strong)AVCaptureSession *session;
 
@@ -23,8 +32,65 @@
     [super viewDidLoad];
     self.title = @"二维码扫描";
     self.view.backgroundColor = [UIColor whiteColor];
+    [self setupPath];
     
     [self setupScanningQRCode];
+}
+
+- (void)setupPath {
+    CAShapeLayer *shapeLayer = [CAShapeLayer layer];
+    shapeLayer.frame = self.view.bounds;
+    
+    UIBezierPath *bezierPath = [UIBezierPath bezierPath];
+    [bezierPath moveToPoint:CGPointMake(0, 0)];
+    [bezierPath addLineToPoint:CGPointMake(0, kHeight)];
+    [bezierPath addLineToPoint:CGPointMake(kWidth, kHeight)];
+    [bezierPath addLineToPoint:CGPointMake(kWidth, 0)];
+    [bezierPath addLineToPoint:CGPointMake(0, 0)];
+
+    [bezierPath addLineToPoint:CGPointMake(rectangleX, rectangleY)];
+    [bezierPath addLineToPoint:CGPointMake(rectangleX + rectangleW, rectangleY)];
+    [bezierPath addLineToPoint:CGPointMake(rectangleX + rectangleW, rectangleY + rectangleH)];
+    [bezierPath addLineToPoint:CGPointMake(rectangleX, rectangleY + rectangleH)];
+    [bezierPath addLineToPoint:CGPointMake(rectangleX, rectangleY)];
+    
+    [bezierPath closePath];
+    [bezierPath fill];
+
+    shapeLayer.path = bezierPath.CGPath;
+    shapeLayer.fillColor = [[UIColor blackColor] colorWithAlphaComponent:0.5].CGColor;
+    [self.view.layer addSublayer:shapeLayer];
+    
+    
+    UIButton *flashBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    flashBtn.frame = CGRectMake(162.5, 600, 50, 50);
+    flashBtn.backgroundColor = [UIColor brownColor];
+//    flashBtn.selected = NO;
+    [flashBtn addTarget:self action:@selector(flashBtnClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:flashBtn];
+}
+
+- (void)flashBtnClick:(UIButton *)sender {
+    sender.selected = !sender.selected;
+    if (sender.isSelected == YES) { //打开闪光灯
+        AVCaptureDevice *captureDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+        NSError *error = nil;
+        
+        if ([captureDevice hasTorch]) {
+            BOOL locked = [captureDevice lockForConfiguration:&error];
+            if (locked) {
+                captureDevice.torchMode = AVCaptureTorchModeOn;
+                [captureDevice unlockForConfiguration];
+            }
+        }
+    }else{//关闭闪光灯
+        AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+        if ([device hasTorch]) {
+            [device lockForConfiguration:nil];
+            [device setTorchMode: AVCaptureTorchModeOff];
+            [device unlockForConfiguration];
+        }
+    }
 }
 
 - (void)setupScanningQRCode {
@@ -38,12 +104,12 @@
     [output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
     // 设置扫描范围（每个取值0~1， 以屏幕右上角为坐标原点，往下为x增，往左为y增，宽高和一般的互换了）
 //    output.rectOfInterest = CGRectMake(0.05, 0.2, 0.7, 0.6);
-//    CGRectMake(<#CGFloat x#>, <#CGFloat y#>, <#CGFloat width#>, <#CGFloat height#>)
     /**
      * 0.5 0 0.5 0.5   右下角
      * 0 0.5 0.5 0.5   左上角
      * 0 0 0.5 0.5     右上角
      * 0.5 0.5 0.5 0.5 左下角*/
+    output.rectOfInterest = CGRectMake(rectangleY/kHeight, rectangleX/kWidth, rectangleH/kHeight, rectangleW/kWidth);
     
     // 5.初始化链接对象（会话对象）
     self.session = [[AVCaptureSession alloc] init];
